66f250969bf8cfdffb2bd6ba29666148f1d053dd
Emmaline Mercer <eewmercer@gmail.com>
2025-09-03 13:37:26 -0400

    Connect quinary functionality to math operations

--- operations.py
+++ operations.py
@@ -12,7 +12,7 @@ def multiplication(num1, num2):
 
 def division(num1, num2):
     if num2 == 0:
-        result = "Infinity"
+        result = "Error"
     else:
-        result = num1 / num2
+        result = num1 // num2
     return result
